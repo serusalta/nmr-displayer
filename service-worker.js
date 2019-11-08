4ac7c3c1e46e4de27b864a054a1544bfe67802b4
Luc Patiny <lpatiny@users.noreply.github.com>
2019-11-08 11:06:11 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,7 @@
  * directly; instead, make changes to your Workbox build configuration
  * and re-run your build process.
  * See https://goo.gl/2aRDsh
- */
+ */ 
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
